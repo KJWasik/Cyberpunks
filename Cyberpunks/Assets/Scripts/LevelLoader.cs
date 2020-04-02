07538752498f47a65cb9f639e364f4711dfabb54
kjwasik <krzysztofwasik1988@wp.pl>
2020-04-02 20:31:40 +0100

    Setting up scenes (Splash Screen, Start Screen). LevelLoader C# file created.

---
+++ Cyberpunks/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float delayInSeconds = 3f;
+
+    int currentSceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitAndLoad());
+        }
+    }
+
+    // Showing Loading Screen for 3 seconds
+    IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
