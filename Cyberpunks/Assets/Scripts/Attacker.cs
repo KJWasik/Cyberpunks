1b11b727cffce3adbe2da52b64d4ef6782019dc9
kjwasik <krzysztofwasik1988@wp.pl>
2020-04-09 20:39:11 +0100

    Spawning enemies. Working on defenders. AttackerSpawner C# file created.

--- Cyberpunks/Assets/Scripts/Attacker.cs
+++ Cyberpunks/Assets/Scripts/Attacker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
-    [SerializeField] [Range(0f, 5f)] float walkSpeed = 1f;
+    float currentSpeed = 0.8f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +15,11 @@ public class Attacker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
     }
 }
