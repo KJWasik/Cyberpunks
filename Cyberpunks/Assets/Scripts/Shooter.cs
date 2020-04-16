0584d29b43d62215b13efc06f321cdf0da341832
kjwasik <krzysztofwasik1988@wp.pl>
2020-04-16 19:22:10 +0100

    Shooting only when attackers are in the same lane. Instantiating only one object per square.

--- Cyberpunks/Assets/Scripts/Shooter.cs
+++ Cyberpunks/Assets/Scripts/Shooter.cs
@@ -6,21 +6,59 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject weapon;
+    Animator animator;
+
+    AttackerSpawner myLaneSpawner;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetLaneSpawner();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsAttackerInLane())
+        {
+            animator.SetBool("isShooting", true);
+        }
+        else
+        {
+            animator.SetBool("isShooting", false);
+        }
     }
 
     public void Fire()
     {
         Instantiate(projectile, weapon.transform.position, weapon.transform.rotation);
     }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }    
 }
