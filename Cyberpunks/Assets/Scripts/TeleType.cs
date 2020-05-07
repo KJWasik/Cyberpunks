3ac1d76d9ee85d46159ecacfec210f1388aea892
kjwasik <krzysztofwasik1988@wp.pl>
2020-05-07 17:57:14 +0100

    Working on splash, menu and options screens. TeleType C# file created.

---
+++ Cyberpunks/Assets/Scripts/TeleType.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TeleType : MonoBehaviour
+{
+    public float letterPause = 0.2f;
+    string message;
+    TextMeshProUGUI textComp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComp = GetComponent<TextMeshProUGUI>();
+        message = textComp.text;
+        textComp.text = "";
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        while (true)
+        {
+            foreach (char letter in message.ToCharArray())
+            {
+                textComp.text += letter;
+                yield return new WaitForSeconds(letterPause);
+            }
+            textComp.text = "";
+        }
+    }
+}
