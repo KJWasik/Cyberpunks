705ebf2d7e9502f6790775970229b556a2b5b7ce
kjwasik <krzysztofwasik1988@wp.pl>
2020-06-27 19:07:10 +0100

    Button click sound added. ButtonSound C# file created.

---
+++ Cyberpunks/Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSound : MonoBehaviour
+{
+    [SerializeField] AudioClip buttonSound;
+    [SerializeField] [Range(0, 1)] float buttonSoundVolume = 0.3f;
+    private Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(() => PlayClickSound());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void PlayClickSound()
+    {
+        if (buttonSound)
+        {
+            AudioSource.PlayClipAtPoint(buttonSound, Camera.main.transform.position, buttonSoundVolume);
+        }
+    }
+}
