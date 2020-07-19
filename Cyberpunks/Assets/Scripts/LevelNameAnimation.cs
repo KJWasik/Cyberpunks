a81f66db6dc3a77f15c4d2ed876a0e8156f4af84
kjwasik <krzysztofwasik1988@wp.pl>
2020-07-19 21:11:05 +0100

    Level name animation added. LevelNameAnimation C# file created.

---
+++ Cyberpunks/Assets/Scripts/LevelNameAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelNameAnimation : MonoBehaviour
+{
+    [SerializeField] GameObject levelNameAnimation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (levelNameAnimation)
+        {
+            GameObject newAnimation = Instantiate(levelNameAnimation, transform.position, Quaternion.identity);
+            Destroy(newAnimation, 2f);
+        }
+    }
+}
